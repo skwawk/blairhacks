006d1de80772333cd734f121104602109c32de55
Lillian <lillianzhoudc@gmail.com>
2017-12-03 00:39:53 +0000

    Add URLs

--- takeinitiative/pages/urls.py
+++ takeinitiative/pages/urls.py
@@ -4,5 +4,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
 	url('^$', TemplateView.as_view(template_name='index.html')),
 	url('about.html', TemplateView.as_view(template_name='about.html')),
+	url('signatures.html', TemplateView.as_view(template_name='signatures.html')),
+	url('petitions.html', TemplateView.as_view(template_name='petitions.html')),
+	url('map.html', TemplateView.as_view(template_name='map.html')),
 ]
 
